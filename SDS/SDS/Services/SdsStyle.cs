1ba7d6ec7780112862d82f25be3283f1b6d6ea06
Aung Myo <aungmyo2123@gmail.com>
2025-05-17 20:38:59 +0630

    feat : add style like css

---
+++ SDS/SDS/Services/SdsStyle.cs
@@ -0,0 +1,12 @@
+﻿namespace SDS.Services
+{
+    public readonly struct Style
+    {
+        public const float Padding = 5;
+        public const float Spacing = 5;
+
+        public const float SectionHeaderFontSize = 12;
+
+        public const float TableBorder = 1;
+    }
+}
