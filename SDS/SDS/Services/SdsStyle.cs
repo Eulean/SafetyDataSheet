5a9c9c676ae940ef62c612dcb87485312cc9c46d
Aung Myo <aungmyo2123@gmail.com>
2025-05-17 20:50:28 +0630

    feat: Header color

--- SDS/SDS/Services/SdsStyle.cs
+++ SDS/SDS/Services/SdsStyle.cs
@@ -1,12 +1,21 @@
-﻿namespace SDS.Services
+﻿using System.Drawing;
+
+namespace SDS.Services
 {
     public readonly struct Style
     {
+        // Global Style 
         public const float Padding = 5;
         public const float Spacing = 5;
 
         public const float SectionHeaderFontSize = 12;
 
         public const float TableBorder = 1;
+
+        public static readonly Color HeaderColor = ColorTranslator.FromHtml("#00285b");
+
+        // Section 1 Style 
+
+        // Section 2 ... etc Please check whether a usable style exists.
     }
 }
